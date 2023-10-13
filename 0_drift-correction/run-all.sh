eb3d05f0149df055a88927325b7567bc6431ec67
Logan Ward <ward.logan.t@gmail.com>
2023-10-13 09:26:18 -0400

    Do not include voids that touch sides in analysis
    Fixes #4

--- 0_drift-correction/run-all.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-for notebook in *_*.ipynb; do
-    echo $notebook
-    for run in ../data/8bitTIFF-*; do
-        echo $run
-        papermill -p run_directory $run $notebook /dev/null
-    done
-done
