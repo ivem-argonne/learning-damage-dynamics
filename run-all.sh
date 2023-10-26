f9f4edee6c814ad92886d3f02e332a11feb7b201
Logan Ward <ward.logan.t@gmail.com>
2023-10-26 10:31:30 -0400

    Use Bayesian parameter estimation, compute critical radii
    Fixes #7

--- run-all.sh
+++ run-all.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
-for notebook in *_*.ipynb; do
+# Determine which notebooks to run
+notebooks=`ls *_*.ipynb`
+if [[ $# -ge 1 ]]; then
+    notebooks=$1
+fi
+
+for notebook in $notebooks; do
     echo $notebook
     for run in ../data/8bitTIFF-*; do
         echo $run
